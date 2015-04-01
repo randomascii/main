2df0de35e8cd15e4eb9694e26b138d0c286e0a7c
Bruce Dawson <brucedawson@chromium.org>
2015-04-01 09:28:25 -0700

    Tweak Chrome process identification script, and add two .exes.
    It's time to add an official build of UIforETW.exe, and RetrieveSymbols.exe.

--- xperf/bin/IdentifyChromeProcesses.py
+++ xperf/bin/IdentifyChromeProcesses.py
@@ -11,16 +11,26 @@ pidRe = re.compile(r".*\(([\d ]*)\),.*")
 processTypeRe = re.compile(r".* --type=([^ ]*) .*")
 
 tracename = sys.argv[1]
-command = 'xperf -i "%s" -tle -tti -a  process -withcmdline' % tracename
+command = 'xperf -i "%s" -tle -tti -a process -withcmdline' % tracename
 # Note the importance of printing the '\r' so that the
 # output will be compatible with Windows edit controls.
-print "Chrome process types by PID:\r"
-print " PID: process type\r"
+print "Chrome PIDs by process type:\r"
+pidsByType = {}
 for line in os.popen(command).readlines():
   if line.count("chrome.exe") > 0:
     match = processTypeRe.match(line)
     type = "browser"
     if match:
-      type = " " + match.groups()[0]
+      type = match.groups()[0]
     pid = int(pidRe.match(line).groups()[0])
-    print "%5d: %s\r" % (pid, type)
+    pidList = pidsByType.get(type, [])
+    pidList.append(pid)
+    pidsByType[type] = pidList
+
+keys = pidsByType.keys()
+keys.sort()
+for type in keys:
+  print "%-10s:" % type,
+  for pid in pidsByType[type]:
+    print "%d" % pid,
+  print "\r"
